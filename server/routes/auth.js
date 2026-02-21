a813e2814bc0536095b5bdbf664f0f2105bcf5b6
chdhanya20 <chdhanya55@gmail.com>
2026-02-21 13:50:07 +0530

    Update auth.js

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -98,8 +98,8 @@ router.post('/login', async (req, res) => {
     res
       .cookie('token', token, {
         httpOnly: true,
-        secure: process.env.NODE_ENV === 'production',
-        sameSite: 'lax',
+        secure: true,          // must be true in production (HTTPS)
+        sameSite: 'None',      // VERY IMPORTANT for cross-domain
         maxAge: 7 * 24 * 60 * 60 * 1000,
         path: '/',
       })
